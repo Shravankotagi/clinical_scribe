a2f8f7eda689bd861fffbcd2cbba06948f229fec
Shravankotagi <your-github-email@gmail.com>
2026-05-21 17:42:21 +0530

    remove search and notification icons from headers

--- auth-app/src/app/layout.tsx
+++ auth-app/src/app/layout.tsx
@@ -25,8 +25,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute='class'
           defaultTheme='light'
-          // enableSystem
-          disableTransitionOnChange
+          enableSystem={false}
         >
           <main>{children}</main>
           <Toaster />
